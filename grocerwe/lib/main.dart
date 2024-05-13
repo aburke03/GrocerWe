b3dd279c739471e61240f8dfb6da2651afba9ad4
aburke03 <aburke2571@gmail.com>
2024-05-12 22:42:55 -0500

    Push generated running files

--- grocerwe/lib/main.dart
+++ grocerwe/lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 5, 204, 18)),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 5, 204, 18)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Home Page'),
